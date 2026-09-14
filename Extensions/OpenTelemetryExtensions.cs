d006fefbe8e81937468d2f5ff0e08fbd3e4ccdee
riskiekalimuda <riskie.kalimuda@gmail.com>
2026-09-14 22:09:12 +0700

    add telemetry

---
+++ Extensions/OpenTelemetryExtensions.cs
@@ -0,0 +1,28 @@
+﻿using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+namespace APIGateway.Msv.Extensions
+{
+    public static class OpenTelemetryExtensions
+    {
+        public static IServiceCollection AddGatewayTelemetry(this IServiceCollection services, IConfiguration configuration)
+        {
+            var serviceName = "ApiGateway-YARP";
+
+            services.AddOpenTelemetry()
+                .ConfigureResource(resource => resource.AddService(serviceName))
+                .WithTracing(tracing =>
+                {
+                    tracing
+                        .AddAspNetCoreInstrumentation(options => options.RecordException = true)
+                        .AddSource("Yarp.ReverseProxy")
+                        .AddOtlpExporter(options =>
+                        {
+                            options.Endpoint = new Uri(configuration["OTEL_EXPORTER_OTLP_ENDPOINT"] ?? "http://localhost:4317");
+                        });
+                });
+
+            return services;
+        }
+    }
+}
